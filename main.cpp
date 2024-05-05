32fef0f1ac1b209b36db3861b4d10d98e9011e88
bartox7777 <bartosz.zelek@protonmail.com>
2024-05-06 01:37:14 +0200

    handle msgs

--- main.cpp
+++ main.cpp
@@ -6,17 +6,41 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: " << argv[0] << " A" << std::endl;
+        std::cout << "Usage: " << argv[0] << " A" << std::endl;
         return 1;
     }
-    int N = std::stoi(argv[1]);
-    int A = std::stoi(argv[2]);
+    int A = std::stoi(argv[1]);
 
     MPI::Init();
     int rank = MPI::COMM_WORLD.Get_rank(); // Get the rank of the current process
     int size = MPI::COMM_WORLD.Get_size(); // Get the total number of processes
 
-    Students students(N, A, rank);
+    Students students(size, A, rank);
+    students.want_to_drink();
+
+    int response;
+    MPI::Status response_status;
+
+    // #pragma omp parallel num_threads(2)
+    while (true)
+    {
+        // #pragma omp single
+        MPI::COMM_WORLD.Recv(&response, 1, MPI::INT, MPI::ANY_SOURCE, MPI::ANY_TAG, response_status);
+
+        switch (response_status.Get_tag())
+        {
+        case MessageType::REQUEST:
+            // #pragma omp single
+            students.handle_request(response_status.Get_source(), response);
+            break;
+        case MessageType::REPLY:
+            // #pragma omp single
+            students.handle_reply(response_status.Get_source(), response);
+            break;
+        default:
+            break;
+        }
+    }
 
     MPI::Finalize();
 
--- students.h
+++ students.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <mpi.h>
 #include "common.h"
+#include <unistd.h>
 
 class Students
 {
@@ -38,13 +39,14 @@ public:
 
     void want_to_drink()
     {
-        std::cout << "[" << local_counter << "] Students " << rank << " wants to drink" << std::endl;
+        std::cerr << "[" << local_counter << "] Students " << rank << " wants to drink" << std::endl;
         state = StudentsState::WANT_TO_DRINK;
         local_counter = global_counter + 1;
         for (unsigned i = 0; i < N; ++i)
         {
             if (i != rank)
             {
+                std::cerr << "[" << local_counter << "] Students " << rank << " sent request to " << i << std::endl;
                 MPI::COMM_WORLD.Send(&local_counter, 1, MPI::INT, i, MessageType::REQUEST);
                 ++reply_counter[i];
             }
@@ -53,14 +55,16 @@ public:
 
     void handle_request(unsigned Y_rank, unsigned Y_counter)
     {
-        std::cout << "[" << local_counter << "] Students " << rank << " received request from " << Y_rank << " with counter " << Y_counter << std::endl;
+        std::cerr << "[" << local_counter << "] Students " << rank << " received request from " << Y_rank << " with counter " << Y_counter << std::endl;
         global_counter = std::max(global_counter, Y_counter);
         if (state == StudentsState::DRINKING || (state == StudentsState::WANT_TO_DRINK) && (local_counter < Y_counter || (local_counter == Y_counter && rank < Y_rank)))
         {
+            std::cerr << "[" << local_counter << "] Students " << rank << " deferred reply to " << Y_rank << std::endl;
             ++defer_counter[Y_rank];
         }
         else
         {
+            std::cerr << "[" << local_counter << "] Students " << rank << " replied to " << Y_rank << std::endl;
             unsigned no_replays = 1;
             MPI::COMM_WORLD.Send(&no_replays, 1, MPI::INT, Y_rank, MessageType::REPLY);
         }
@@ -76,23 +80,26 @@ public:
                 ++not_drinking;
             }
         }
+        std::cerr << "[" << local_counter << "] Students " << rank << " count not drinking " << not_drinking << std::endl;
         return not_drinking;
     }
 
     void handle_reply(unsigned Y_rank, unsigned no_replays)
     {
-        std::cout << "[" << local_counter << "] Students " << rank << " received reply from " << Y_rank << " with no_replays " << no_replays << std::endl;
+        std::cerr << "[" << local_counter << "] Students " << rank << " received reply from " << Y_rank << " with no_replays " << no_replays << std::endl;
         reply_counter[Y_rank] -= no_replays;
         if (state == StudentsState::WANT_TO_DRINK && count_not_drinking() >= N - A)
         {
             state = StudentsState::DRINKING;
-            std::cout << "Students " << rank << " is drinking" << std::endl;
+            std::cerr << "Students " << rank << " is drinking" << std::endl;
+            sleep(5);
+            stop_drinking();
         }
     }
 
     void stop_drinking()
     {
-        std::cout << "[" << local_counter << "] Students " << rank << " stopped drinking" << std::endl;
+        std::cerr << "[" << local_counter << "] Students " << rank << " stopped drinking" << std::endl;
         state = StudentsState::IDLE;
         for (unsigned i = 0; i < N; ++i)
         {
